37833021ebce0d7597f340e2ef24f08d2e133a97
ruscoe <danruscoe@gmail.com>
2017-07-14 00:58:37 -0700

    Add fields to Newsletter Signup block.

--- modules/bene_core/src/Plugin/Block/NewsletterSignupBlock.php
+++ modules/bene_core/src/Plugin/Block/NewsletterSignupBlock.php
@@ -19,8 +19,40 @@ class NewsletterSignupBlock extends BlockBase {
   /**
    * {@inheritdoc}
    */
+  public function defaultConfiguration() {
+    return [
+      'style' => '1',
+      'title' => 'Sign up for our Newsletter',
+      'signup_text' => 'Receive updates about what we are doing',
+    ] + parent::defaultConfiguration();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function blockForm($form, FormStateInterface $form_state) {
-    $form = [];
+    $form['style'] = [
+      '#type' => 'radios',
+      '#title' => $this->t('Style'),
+      '#default_value' => $this->configuration['style'],
+      '#options' => [
+        0 => $this->t('Disabled'),
+        1 => $this->t('External'),
+        2 => $this->t('Embedded Form'),
+      ],
+    ];
+    $form['title'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Title'),
+      '#description' => '',
+      '#default_value' => $this->configuration['title'],
+    ];
+    $form['signup_text'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Signup text'),
+      '#default_value' => $this->configuration['signup_text'],
+    ];
+
     return $form;
   }
 
@@ -28,7 +60,9 @@ class NewsletterSignupBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function blockSubmit($form, FormStateInterface $form_state) {
-
+    $this->configuration['style'] = $form_state->getValue('style');
+    $this->configuration['title'] = $form_state->getValue('title');
+    $this->configuration['signup_text'] = $form_state->getValue('signup_text');
   }
 
   /**
