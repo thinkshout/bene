ac6985aa0e4b2f47bfc3bea0d0cc147334a81bf2
ruscoe <danruscoe@gmail.com>
2017-07-26 17:54:59 -0700

    Add breadcrumbs, page title to Sub Navigation block.

--- modules/bene_core/src/Plugin/Block/SubNavigationBlock.php
+++ modules/bene_core/src/Plugin/Block/SubNavigationBlock.php
@@ -4,6 +4,7 @@ namespace Drupal\bene_core\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Link;
 
 /**
  * Provides a 'SubNavigationBlock' block.
@@ -45,7 +46,55 @@ class SubNavigationBlock extends BlockBase {
   public function build() {
     $build = [];
 
+    // Add breadcrumbs excluding current page.
+    $breadcrumbs = $this->getBreadcrumbs();
+    $breadcrumb_links = [];
+    /** @var Link $breadcrumb */
+    foreach ($breadcrumbs as $breadcrumb) {
+      $breadcrumb_links[] = Link::fromTextAndUrl($breadcrumb->getText(), $breadcrumb->getUrl());
+    }
+
+    $build['breadcrumbs'] = [
+      '#theme' => 'item_list',
+      '#items' => $breadcrumb_links,
+    ];
+
+    $build['page_title'] = [
+      '#type' => 'markup',
+      '#markup' => $this->getPageTitle(),
+    ];
+
+    $build['page_children'] = [];
+
     return $build;
+  }
+
+  /**
+   * Gets an array of breadcrumb links up to (not including) the current page.
+   *
+   * @return array
+   *   Array of Link objects representing breadcrumbs.
+   */
+  private function getBreadcrumbs() {
+    /** @var \Drupal\Core\Routing\RouteMatch $current_route */
+    $current_route = \Drupal::service('current_route_match')->getCurrentRouteMatch();
+    $breadcrumbs = \Drupal::service('breadcrumb')->build($current_route)->getLinks();
+
+    return $breadcrumbs;
+  }
+
+  /**
+   * Gets the title of the current page.
+   *
+   * @return string
+   *   The page title.
+   */
+  private function getPageTitle() {
+    /** @var \Drupal\Core\Routing\RouteMatch $current_route */
+    $current_route = \Drupal::service('current_route_match')->getCurrentRouteMatch();
+    $title = \Drupal::service('title_resolver')->getTitle(\Drupal::request(), $current_route->getRouteObject());
+
+    return $title;
   }
 
 }
