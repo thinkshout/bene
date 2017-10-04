a77020306c0b9c4f6d8c4d7c4eb8872b267f74fc
Tauno Hogue <tauno@thinkshout.com>
2017-10-04 11:17:33 -0700

    Revise logic in subnav block

--- modules/bene_core/src/Plugin/Block/SubNavigationBlock.php
+++ modules/bene_core/src/Plugin/Block/SubNavigationBlock.php
@@ -3,11 +3,10 @@
 namespace Drupal\bene_core\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
-use Drupal\Core\Breadcrumb\BreadcrumbManager;
-use Drupal\Core\Controller\TitleResolver;
+use Drupal\Core\Cache\Cache;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Link;
 use Drupal\Core\Menu\MenuLinkManager;
+use Drupal\Core\Menu\MenuLinkTreeInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Routing\CurrentRouteMatch;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -31,24 +30,17 @@ class SubNavigationBlock extends BlockBase implements ContainerFactoryPluginInte
   protected $currentRouteMatch;
 
   /**
-   * BreadcrumbManager service.
-   *
-   * @var \Drupal\Core\Breadcrumb\BreadcrumbManager
-   */
-  protected $breadcrumbManager;
-
-  /**
-   * TitleResolver service.
-   *
-   * @var \Drupal\Core\Controller\TitleResolver
-   */
-  protected $titleResolver;
-
-  /**
    * MenuLinkManager service.
    *
    * @var \Drupal\Core\Menu\MenuLinkManager*/
   protected $menuLinkManager;
+
+  /**
+   * The menu link tree service.
+   *
+   * @var \Drupal\Core\Menu\MenuLinkTreeInterface
+   */
+  protected $menuTree;
 
   /**
    * Constructs a new SubNavigationBlock.
@@ -61,26 +53,22 @@ class SubNavigationBlock extends BlockBase implements ContainerFactoryPluginInte
    *   The plugin implementation definition.
    * @param \Drupal\Core\Routing\CurrentRouteMatch $currentRouteMatch
    *   The CurrentRouteMatch service.
-   * @param \Drupal\Core\Breadcrumb\BreadcrumbManager $breadcrumbManager
-   *   The BreadcrumbManager service.
-   * @param \Drupal\Core\Controller\TitleResolver $titleResolver
-   *   The TitleResolver service.
    * @param \Drupal\Core\Menu\MenuLinkManager $menuLinkManager
    *   The MenuLinkManager service.
+   * @param \Drupal\Core\Menu\MenuLinkTreeInterface $menuTree
+   *   The menu tree service.
    */
   public function __construct(array $configuration,
                               $plugin_id,
                               $plugin_definition,
                               CurrentRouteMatch $currentRouteMatch,
-                              BreadcrumbManager $breadcrumbManager,
-                              TitleResolver $titleResolver,
-                              MenuLinkManager $menuLinkManager) {
+                              MenuLinkManager $menuLinkManager,
+                              MenuLinkTreeInterface $menuTree) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
 
     $this->currentRouteMatch = $currentRouteMatch;
-    $this->breadcrumbManager = $breadcrumbManager;
-    $this->titleResolver = $titleResolver;
     $this->menuLinkManager = $menuLinkManager;
+    $this->menuTree = $menuTree;
   }
 
   /**
@@ -92,9 +80,8 @@ class SubNavigationBlock extends BlockBase implements ContainerFactoryPluginInte
       $plugin_id,
       $plugin_definition,
       $container->get('current_route_match'),
-      $container->get('breadcrumb'),
-      $container->get('title_resolver'),
-      $container->get('plugin.manager.menu.link')
+      $container->get('plugin.manager.menu.link'),
+      $container->get('menu.link_tree')
     );
   }
 
@@ -125,147 +112,72 @@ class SubNavigationBlock extends BlockBase implements ContainerFactoryPluginInte
    * {@inheritdoc}
    */
   public function build() {
-    // Disable block cache as page title will change per-page.
-    // TODO: Review this for potential caching with tags based on path.
-    $build = [
-      '#cache' => [
-        'max-age' => 0,
-      ],
+    $menu_links = $this->menuLinkManager->loadLinksByRoute($this->currentRouteMatch->getRouteName(), $this->currentRouteMatch->getRawParameters()->all());
+    $menu_link = reset($menu_links);
+    $menu_name = $this->getMenuName();
+
+    $parameters = $this->menuTree->getCurrentRouteMenuTreeParameters($menu_name);
+    // Only show one level.
+    $parameters->setMaxDepth(1);
+    // We default to the current item as root.
+    $parameters->setRoot($menu_link->getPluginId());
+
+    // Store initial parameters since menuTree->load alters them.
+    $fallback_parameters = clone $parameters;
+
+    $tree = $this->menuTree->load($menu_name, $parameters);
+
+    // Check if the returned tree has children. If it doesn't get a new tree
+    // starting with the parent of the current item.
+    $tree_item = reset($tree);
+    if (!$tree_item->hasChildren && $menu_link->getParent()) {
+      $fallback_parameters->setRoot($menu_link->getParent());
+      $tree = $this->menuTree->load($menu_name, $fallback_parameters);
+    }
+
+    $manipulators = [
+      ['callable' => 'menu.default_tree_manipulators:checkAccess'],
+      ['callable' => 'menu.default_tree_manipulators:generateIndexAndSort'],
     ];
+    $tree = $this->menuTree->transform($tree, $manipulators);
+    return $this->menuTree->build($tree);
 
-    // Add breadcrumbs excluding current page.
-    $breadcrumbs = $this->getBreadcrumbs();
-    $breadcrumb_links = [];
-    /** @var \Drupal\Core\Link $breadcrumb */
-    foreach ($breadcrumbs as $breadcrumb) {
-      $breadcrumb_links[] = Link::fromTextAndUrl($breadcrumb->getText(), $breadcrumb->getUrl());
-    }
-
-    $build['breadcrumbs'] = [
-      '#theme' => 'item_list',
-      '#wrapper_attributes' => [
-        'class' => [
-          'bene-subnav-breadcrumbs',
-        ],
-      ],
-      '#items' => $breadcrumb_links,
-    ];
-
-    // Add current page title.
-    $page_title = $this->getPageTitle();
-    // Account for the page title being returned as either a string or render
-    // array.
-    if (is_array($page_title)) {
-      $build['page_title'] = $page_title;
-    }
-    else {
-      $build['page_title'] = [
-        '#type' => 'markup',
-        '#markup' => $this->getPageTitle(),
-        '#prefix' => '<div class="bene-subnav-page-title">',
-        '#suffix' => '</div>',
-      ];
-    }
-
-    $page_child_links = [];
-
-    $page_children = $this->getChildMenuLinks();
-    if (!empty($page_children)) {
-      foreach ($page_children as $page_child) {
-        $page_child_links[] = Link::createFromRoute($page_child['title'], $page_child['route_name'], $page_child['route_parameters']);
-      }
-
-      $build['page_children'] = [
-        '#theme' => 'item_list',
-        '#wrapper_attributes' => [
-          'class' => [
-            'bene-subnav-children',
-          ],
-        ],
-        '#items' => $page_child_links,
-      ];
-    }
-
-    return $build;
   }
 
   /**
-   * Gets an array of breadcrumb links up to (not including) the current page.
-   *
-   * @return array
-   *   Array of Link objects representing breadcrumbs.
+   * {@inheritdoc}
    */
-  private function getBreadcrumbs() {
-    $current_route = $this->currentRouteMatch->getCurrentRouteMatch();
-    $breadcrumbs = $this->breadcrumbManager->build($current_route)->getLinks();
-
-    // Remove 'Home' link from breadcrumbs.
-    if (isset($breadcrumbs[0]) && $breadcrumbs[0]->getUrl()->getRouteName() == '<front>') {
-      unset($breadcrumbs[0]);
-      // Reset array indexes.
-      $breadcrumbs = array_values($breadcrumbs);
-    }
-
-    return $breadcrumbs;
+  public function getCacheTags() {
+    // Even when the menu block renders to the empty string for a user, we want
+    // the cache tag for this menu to be set: whenever the menu is changed, this
+    // menu block must also be re-rendered for that user, because maybe a menu
+    // link that is accessible for that user has been added.
+    $cache_tags = parent::getCacheTags();
+    $cache_tags[] = 'config:system.menu.' . $this->getMenuName();
+    return $cache_tags;
   }
 
   /**
-   * Gets the title of the current page.
-   *
-   * @return array|string
-   *   The page title as a string or render array.
-   *
-   * @see https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Controller%21TitleResolver.php/class/TitleResolver/8.2.x
+   * {@inheritdoc}
    */
-  private function getPageTitle() {
-    $current_route = $this->currentRouteMatch->getCurrentRouteMatch();
-    $title = $this->titleResolver->getTitle(\Drupal::request(), $current_route->getRouteObject());
-
-    return $title;
+  public function getCacheContexts() {
+    // ::build() uses MenuLinkTreeInterface::getCurrentRouteMenuTreeParameters()
+    // to generate menu tree parameters, and those take the active menu trail
+    // into account. Therefore, we must vary the rendered menu by the active
+    // trail of the rendered menu.
+    // Additional cache contexts, e.g. those that determine link text or
+    // accessibility of a menu, will be bubbled automatically.
+    $menu_name = $this->getMenuName();
+    return Cache::mergeContexts(parent::getCacheContexts(), ['route.menu_active_trails:' . $menu_name]);
   }
 
   /**
-   * Gets an array of menu link definitions for child links of the current item.
-   *
-   * Based on the current entity's menu link.
-   *
-   * @return array
-   *   Array of menu link definitions (each definition is in array format).
-   *
-   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * Get the menu name for the current page.
    */
-  private function getChildMenuLinks() {
-    $child_menu_links = [];
-
-    // Get the current entity from the current route.
-    $route = \Drupal::routeMatch();
-    $route_name = $route->getRouteName();
-    $route_parameters = \Drupal::routeMatch()->getParameters()->all();
-    $route_entity = reset($route_parameters);
-
-    if (!empty($route_entity) && is_object($route_entity)) {
-      $menu_links = $this->menuLinkManager->loadLinksByRoute($route_name, [$route_entity->getEntityTypeId() => $route_entity->id()]);
-
-      // Get the menu link associated with the current entity.
-      /** @var \Drupal\menu_link_content\Plugin\Menu\MenuLinkContent $current_menu_link */
-      $current_menu_link = reset($menu_links);
-
-      // Get all child menu links of the current entity's menu link.
-      if (!empty($current_menu_link)) {
-        $child_menu_ids = $this->menuLinkManager->getChildIds($current_menu_link->getPluginId());
-
-        foreach ($child_menu_ids as $menu_link_id) {
-          $child_menu_link = $this->menuLinkManager->getDefinition($menu_link_id);
-
-          // Only display first-level child links.
-          if ($child_menu_link['parent'] == $current_menu_link->getPluginId()) {
-            $child_menu_links[] = $this->menuLinkManager->getDefinition($menu_link_id);
-          }
-        }
-      }
-    }
-
-    return $child_menu_links;
+  private function getMenuName() {
+    $menu_links = $this->menuLinkManager->loadLinksByRoute($this->currentRouteMatch->getRouteName(), $this->currentRouteMatch->getRawParameters()->all());
+    $menu_link = reset($menu_links);
+    return $menu_link->getMenuName();
   }
 
 }
