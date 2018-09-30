21c980f28f7e89bd6718150cde83f2c2a3a058d7
rjacobsen0 <rjacobsen0@gmail.com>
2018-09-29 20:50:04 -0700

    handling the case when there's some calculation to be done

--- themes/bene_parent/sass/gen_c_level_vars.sh
+++ themes/bene_parent/sass/gen_c_level_vars.sh
@@ -22,11 +22,14 @@ for x in $(ls ./components/*.scss);
   #    @include set-bbv-var(line-height, input-height - 2px, --drop_down-line-height);
   #        @include set-bbv-var(color, font-color-accent, --general-font-accent-color);
   #          @include set-bbv-var(border-color, disabled-border-color, --drop_down-disabled-after-border-color);
-  #
-  # for lines that look like a path and filename, output two slashes and a space followed
-  # by everything after the last slash underscore from the input. FYI \'$'\n'' means newline - we are using use an ANSI C-quoted string ($'...') to splice in the newline $'\n'
+
+  # For lines that look like a path and filename, output two slashes and a space followed
+  # by everything after the last slash underscore from the input. FYI \'$'\n'' means newline - we are using use
+  # an ANSI C-quoted string ($'...') to splice in the newline $'\n'
   # then removing the filename extension (everything after the last dot) and adding a newline
-  do echo $x | sed 's/.*\/_/\'$'\n''\/\/ /' | sed 's/\.[^.]*$/\'$'\n''/';
+  do echo $x | \
+  sed 's/.*\/_/\'$'\n''\/\/ /' | \
+  sed 's/\.[^.]*$/\'$'\n''/';
 
   # for lines that have set-bbv-var in them like this
   #
@@ -38,7 +41,19 @@ for x in $(ls ./components/*.scss);
   #
   # because this sets the new c-level variable to the value of the b-level variable. Then use that output
   # to create the c-level css variables.
-  cat $x | grep 'set-bbv-var' | sed 's/ *@include set-bbv-var(\(.*\),[ \t]*\(.*\),[ \t]*\(.*\));/\3: var(--\2);/g';
+  #
+  # And finally we look at the few lines that contain calculations. For example we might get a line that looks like this
+  #
+  # --drop_down-small-line-height: var(--input-height-small - 2px);
+  #
+  # If we get one of these we want to change it to
+  #
+  # --drop_down-small-line-height: calc(var(--input-height-small) - 2px);
+  #
+  # note: the square brackets below contain both a space and a tab but for some reason \s was not working.
+  cat $x | grep 'set-bbv-var' | \
+  sed 's/ *@include set-bbv-var(\(.*\),[    ]*\(.*\),[  ]*\(.*\));/\3: var(--\2);/g' | \
+  sed 's/\([^     ]*\):[    ]*var(\(.*\)--\([^    ]*\) [    ]*\(.*\));/\1: calc(\2 var(--\3) \4);/';
 
 done
 
