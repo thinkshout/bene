126e48d0fa789206e8d1c81298abd42f9a82d39c
Samuel Mortenson <samuel@mortenson.coffee>
2019-06-05 11:14:18 -0700

    Testing

--- tests/behat/features/bootstrap/FeatureContext.php
+++ tests/behat/features/bootstrap/FeatureContext.php
@@ -11,31 +11,8 @@ class FeatureContext extends RawDrupalContext implements Context, SnippetAccepti
   const DEFAULT_PASSWORD = "admin";
 
   /**
-   * @var int
-   *   The ID of the student currently being used by a test.
-   */
-  private $current_student_id;
-
-  /**
-   * Initializes context.
-   * Every scenario gets its own context object.
+   * Asserts that a user is logged in.
    *
-   * @param array $parameters
-   *   Context parameters (set them in behat.yml)
-   */
-  public function __construct(array $parameters = []) {
-    // Initialize your context here
-  }
-
-  /**
-   * @AfterScenario @migration
-   */
-  public function afterScenario(\Behat\Behat\Hook\Scope\AfterScenarioScope $scope) {
-    $driver = $this->getDriver('drush');
-    $driver->mr('--all');
-  }
-
-  /**
    * @Given I am logged in as user with email :email
    */
   public function assertLoggedInAsUser($email) {
@@ -58,55 +35,7 @@ class FeatureContext extends RawDrupalContext implements Context, SnippetAccepti
   }
 
   /**
-   * @Given I am testing student :name
-   */
-  public function assertTestingStudent($name) {
-    $this->current_student_id = $this->getStudentIdFromName($name);
-  }
-
-  /**
-   * @When I visit student path :path
-   */
-  public function assertViewingStudentPath($path) {
-    $this->getSession()->visit($this->locatePath('/student/' . $this->current_student_id . '/' . $path));
-  }
-
-  /**
-   * @When I view student :name
-   */
-  public function assertViewingStudentOverview($name)
-  {
-    $student_id = $this->getStudentIdFromName($name);
-
-    $this->getSession()->visit($this->locatePath('/student/' . $student_id));
-  }
-
-  /**
-   * @When I view :tab for student :name
-   */
-  public function assertViewingStudentTab($tab, $name) {
-    $student_id = $this->getStudentIdFromName($name);
-
-    $map = [
-      'add-placement' => 'student-info/add-placement',
-      'add-sibling' => 'student-info/add-sibling',
-      'placements' => 'student-info#edit-group-placement',
-      'siblings' => 'student-info#edit-group-siblings',
-    ];
-
-    if (array_key_exists($tab, $map)) {
-      $url = $map[$tab];
-    }
-    else {
-      $url = $tab;
-    }
-
-    $this->getSession()->visit($this->locatePath('/student/' . $student_id . '/' . $url));
-  }
-
-  /**
-   * Checks that access was denied for a page based on either status code or
-   * "Access Denied." in the error message.
+   * Checks that access was denied for a page.
    *
    * @Then I should get an access denied error
    */
@@ -129,6 +58,8 @@ class FeatureContext extends RawDrupalContext implements Context, SnippetAccepti
   }
 
   /**
+   * Waits for a progress bar to finish.
+   *
    * @Given I wait for the progress bar to finish
    */
   public function iWaitForTheProgressBarToFinish() {
@@ -136,6 +67,8 @@ class FeatureContext extends RawDrupalContext implements Context, SnippetAccepti
   }
 
   /**
+   * Follows a meta refresh tag.
+   *
    * @Given I follow meta refresh
    *
    * https://www.drupal.org/node/2011390
@@ -149,77 +82,24 @@ class FeatureContext extends RawDrupalContext implements Context, SnippetAccepti
   }
 
   /**
-   * @Given I have wiped the site
-   */
-  public function iHaveWipedTheSite()
-  {
-    $site = getenv('PSITE');
-    $env = getenv('PENV');
-
-    passthru("terminus --yes --site=$site --env=$env site wipe");
-  }
-
-  /**
-   * @Given I have reinstalled :arg1
-   */
-  public function iHaveReinstalled($arg1)
-  {
-    $site = getenv('PSITE');
-    $env = getenv('PENV');
-    passthru("drush @pantheon.$site.$env --strict=0 site-install --yes standard --site-name='$arg1' --account-name=admin");
-  }
-
-  /**
-   * @Given I have run the migration
-   */
-  public function iHaveRunMigration() {
-    $driver = $this->getDriver('drush');
-    // Revert migration first so migrated contacts connect with new users.
-    $driver->mr('--all');
-    $this->drushOutput = $driver->mi('--all');
-    if (!isset($this->drushOutput)) {
-      $this->drushOutput = TRUE;
-    }
-  }
-
-  /**
-   * @Given I have committed my changes with comment :arg1
-   */
-  public function iHaveCommittedMyChangesWithComment($arg1)
-  {
-    $site = getenv('PSITE');
-    $env = getenv('PENV');
-
-    passthru("terminus --yes --site=$site --env=$env site code commit --message='$arg1'");
-  }
-
-  /**
-   * @Given I have exported configuration
-   */
-  public function iHaveExportedConfiguration()
-  {
-    $site = getenv('PSITE');
-    $env = getenv('PENV');
-    passthru("drush @pantheon.$site.$env --strict=0 config-export --yes");
-  }
-
-  /**
+   * Waits a certain amount of seconds.
+   *
    * @Given I wait :seconds seconds
    */
-  public function iWaitSeconds($seconds)
-  {
+  public function iWaitSeconds($seconds) {
     sleep($seconds);
   }
 
   /**
+   * Waits for an element to appear.
+   *
    * @Given I wait :seconds seconds or until I see :text
    */
-  public function iWaitSecondsOrUntilISee($seconds, $text)
-  {
-    $errorNode = $this->spin( function($context) use($text) {
-      $node = $context->getSession()->getPage()->find('named', array('content', $text));
+  public function iWaitSecondsOrUntiliSee($seconds, $text) {
+    $errorNode = $this->spin(function ($context) use ($text) {
+      $node = $context->getSession()->getPage()->find('named', ['content', $text]);
       if (!$node) {
-        return false;
+        return FALSE;
       }
       return $node->isVisible();
     }, $seconds);
@@ -230,12 +110,22 @@ class FeatureContext extends RawDrupalContext implements Context, SnippetAccepti
     }
   }
 
-  // http://docs.behat.org/en/v2.5/cookbook/using_spin_functions.html
-  // http://mink.behat.org/en/latest/guides/traversing-pages.html#selectors
-  public function spin ($lambda, $wait = 60)
-  {
-    for ($i = 0; $i <= $wait; $i++)
-    {
+  /**
+   * Displays a spinner until a timer runs out or until a callback return TRUE.
+   *
+   * @param callable $lambda
+   *   A callback.
+   * @param int $wait
+   *   How many seconds to wait. Defaults to 60.
+   *
+   * @return \Behat\Mink\Element\NodeElement|bool
+   *   An error node if available, or TRUE if successful.
+   *
+   * @throws \Exception
+   *   Throws an exception if the timeout is reached.
+   */
+  public function spin(callable $lambda, $wait = 60) {
+    for ($i = 0; $i <= $wait; $i++) {
       if ($i > 0) {
         sleep(1);
       }
@@ -245,15 +135,16 @@ class FeatureContext extends RawDrupalContext implements Context, SnippetAccepti
 
       try {
         if ($lambda($this)) {
-          return true;
+          return TRUE;
         }
-      } catch (Exception $e) {
-        // do nothing
+      }
+      catch (Exception $e) {
+        // Do nothing.
       }
 
       // If we do not see the text we are waiting for, fail fast if
       // we see a Drupal 8 error message pane on the page.
-      $node = $this->getSession()->getPage()->find('named', array('content', 'Error'));
+      $node = $this->getSession()->getPage()->find('named', ['content', 'Error']);
       if ($node) {
         $errorNode = $this->getSession()->getPage()->find('css', '.messages--error');
         if ($errorNode) {
@@ -273,29 +164,6 @@ class FeatureContext extends RawDrupalContext implements Context, SnippetAccepti
       "Timeout thrown by " . $backtrace[1]['class'] . "::" . $backtrace[1]['function'] . "()\n" .
       $backtrace[1]['file'] . ", line " . $backtrace[1]['line']
     );
-
-    return false;
   }
 
-  private function getStudentIdFromName($name) {
-    /** @var \Drupal\Driver\DrushDriver $drush */
-    $drush = $this->getDrupal()->getDriver('drush');
-
-    $conditions = array(
-      array(
-        'field' => 'name',
-        'operator' => '=',
-        'value' => $name,
-      ),
-    );
-
-    $student_ids = $drush->entityQuery('redhen_org', $conditions);
-    if (!empty($student_ids)) {
-      // Limit to first student found.
-      /** @var \Drupal\redhen_org\Entity\Org $student */
-      return current($student_ids);
-    }
-
-    return NULL;
-  }
 }
